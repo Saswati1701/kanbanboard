64941ec48e194e6398307113b848463a1af26d40
Saswati Somya Mahanta <saswatiisbella2002@gmail.com>
2023-10-17 20:19:03 +0530

    repo initialized and added requirements in readme

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import data from './api/data.json'
+
+function App() {
+  // log the imported data
+  console.log(data)
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
